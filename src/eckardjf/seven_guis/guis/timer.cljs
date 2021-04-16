e7e85820a6038622523fd6b895d325c39a7481df
Joseph Eckard <eckardjf@gmail.com>
2021-04-16 00:54:33 -0400

    change timer to use lifecycle methods

--- src/eckardjf/seven_guis/guis/timer.cljs
+++ src/eckardjf/seven_guis/guis/timer.cljs
@@ -10,25 +10,39 @@
         stop-timer! (fn []
                       (js/clearInterval @interval)
                       (reset! interval nil))]
-    (fn []
-      (if (>= @elapsed @duration)
-        (stop-timer!)
-        (when (nil? @interval)
-          (start-timer!)))
-      [:div.flex.flex-col.space-y-4.w-48.text-green-900
-       [:div
-        [:p.mb-1 [:span.font-semibold "Elapsed time: "] (.toFixed @elapsed 1)]
-        [:div.h-4.rounded.overflow-hidden.bg-green-100
-         [:div.w-full.h-full.origin-left.bg-green-400
-          {:style {:transform (str "scaleX(" (/ @elapsed @duration) ")")}}]]]
-       [:div
-        [:p [:span.font-semibold "Duration: "] @duration]
-        [:input.w-full.h-1.appearance-none.outline-none.rounded.slider-thumb.bg-green-400
-         {:type      "range"
-          :min       1
-          :max       60
-          :value     @duration
-          :on-change (fn [e] (reset! duration (js/Number (.. e -target -value))))}]]
-       [:button.btn.bg-green-100.hover:bg-green-200.w-full
-        {:on-click (fn [_] (reset! elapsed 0))}
-        "Reset"]])))
+    (r/create-class
+      {:component-name
+       "timer"
+
+       :component-did-mount
+       start-timer!
+
+       :reagent-render
+       (fn []
+         [:div.flex.flex-col.space-y-4.w-48.text-green-900
+          [:div
+           [:p.mb-1 [:span.font-semibold "Elapsed time: "] (.toFixed @elapsed 1)]
+           [:div.h-4.rounded.overflow-hidden.bg-green-100
+            [:div.w-full.h-full.origin-left.bg-green-400
+             {:style {:transform (str "scaleX(" (/ @elapsed @duration) ")")}}]]]
+          [:div
+           [:p [:span.font-semibold "Duration: "] @duration]
+           [:input.w-full.h-1.appearance-none.outline-none.rounded.slider-thumb.bg-green-400
+            {:type      "range"
+             :min       1
+             :max       60
+             :value     @duration
+             :on-change (fn [e] (reset! duration (js/Number (.. e -target -value))))}]]
+          [:button.btn.bg-green-100.hover:bg-green-200.w-full
+           {:on-click (fn [_] (reset! elapsed 0))}
+           "Reset"]])
+
+       :component-did-update
+       (fn []
+         (if (>= @elapsed @duration)
+           (stop-timer!)
+           (when (nil? @interval)
+             (start-timer!))))
+
+       :component-will-unmount
+       stop-timer!})))
